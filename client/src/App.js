d8613d5dfe22028fa0d7c913731895775c7f39ba
Pratham Rathour <prathamrathour2018@gmail.com>
2021-08-11 15:13:16 +0530

    database connected and schema created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,42 @@
 import React from "react";
-
+import useStyles from "./styles";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import memories from "./img/memories.png";
 const App = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxwidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} varirant="h2" align="center">
+          Memories
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        ></img>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
